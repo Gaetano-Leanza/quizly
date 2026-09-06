a3f67760a9adbb4f5bc7e46b406f7a8c84a4a765
Gaetano-Leanza <gaetano1981@live.de>
2026-09-06 21:16:34 +0200

    feat: setup JWT login and logout with http-only cookies

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer
+from django.contrib.auth import authenticate
 
 
 class RegisterView(APIView):
@@ -13,3 +15,66 @@ class RegisterView(APIView):
             return Response({"message": "Benutzer erfolgreich registriert."}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            response = Response(
+                {"message": "Erfolgreich eingeloggt."}, status=status.HTTP_200_OK)
+
+            response.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=False,
+                samesite='Lax',
+                max_age=3600
+            )
+
+            response.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True,
+                secure=False,
+                samesite='Lax',
+                max_age=86400
+            )
+            return response
+        else:
+
+            return Response(
+                {"detail": "Ungültige Anmeldedaten."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+
+            refresh_token = request.COOKIES.get('refresh_token')
+
+            if refresh_token:
+
+                token = RefreshToken(refresh_token)
+                token.blacklist()
+
+            response = Response(
+                {"message": "Erfolgreich abgemeldet."}, status=status.HTTP_200_OK)
+            response.delete_cookie('access_token')
+            response.delete_cookie('refresh_token')
+
+            return response
+
+        except Exception as e:
+            return Response({"detail": "Fehler beim Logout."}, status=status.HTTP_400_BAD_REQUEST)
