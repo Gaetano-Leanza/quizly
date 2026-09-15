7692fd80e8c432f9bfdd814412e513a23a5b9923
Gaetano-Leanza <gaetano1981@live.de>
2026-09-15 09:18:25 +0200

    fix views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,72 +1,23 @@
-from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer
 from django.contrib.auth import authenticate
-from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer
 
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-
             return Response({"detail": "User created successfully!"}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            refresh_token = str(refresh)
-
-            response_data = {
-                "detail": "Login successfully!",
-                "user": {
-                    "id": user.id,
-                    "username": user.username,
-                    "email": user.email
-                }
-            }
-
-            response = Response(response_data, status=status.HTTP_200_OK)
-
-            response.set_cookie(
-                key='access_token',
-                value=access_token,
-                httponly=True,
-                secure=False,
-                samesite='Lax',
-                max_age=3600
-            )
-
-            response.set_cookie(
-                key='refresh_token',
-                value=refresh_token,
-                httponly=True,
-                secure=False,
-                samesite='Lax',
-                max_age=86400
-            )
-            return response
-        else:
-
-            return Response(
-                {"detail": "Ungültige Anmeldedaten."},
-                status=status.HTTP_401_UNAUTHORIZED
-            )
 
 
 class LoginView(APIView):
@@ -95,6 +46,7 @@ class LoginView(APIView):
 
             response = Response(response_data, status=status.HTTP_200_OK)
 
+            # 3. Cookies setzen
             response.set_cookie(
                 key='access_token',
                 value=access_token,
@@ -114,7 +66,6 @@ class LoginView(APIView):
 
             return response
         else:
-
             return Response(
                 {"detail": "Ungültige Anmeldedaten."},
                 status=status.HTTP_401_UNAUTHORIZED
@@ -122,20 +73,18 @@ class LoginView(APIView):
 
 
 class CookieTokenRefreshView(APIView):
+    permission_classes = [AllowAny]
 
     def post(self, request):
-
         refresh_token = request.COOKIES.get('refresh_token')
 
         if not refresh_token:
-
             return Response(
                 {"detail": "Refresh Token fehlt."},
                 status=status.HTTP_401_UNAUTHORIZED
             )
 
         try:
-
             token = RefreshToken(refresh_token)
             access_token = str(token.access_token)
 
@@ -155,8 +104,20 @@ class CookieTokenRefreshView(APIView):
             return response
 
         except Exception:
-
             return Response(
                 {"detail": "Refresh Token ungültig."},
                 status=status.HTTP_401_UNAUTHORIZED
             )
+
+
+class LogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        response = Response(
+            {"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
+
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+
+        return response
